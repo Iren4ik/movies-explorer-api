d32c407f2ab483cc20b0fb5bfc75d2b8db5c57c9
Iren4ik <124435764+Iren4ik@users.noreply.github.com>
2023-11-16 22:00:24 +0300

    FIX: валидация запроса на удаление карточки

--- middlewares/validation.js
+++ middlewares/validation.js
@@ -40,18 +40,8 @@ const createMovieValidator = celebrate({
 });
 
 const deleteSavedMovieValidator = celebrate({
-  body: Joi.object().keys({
-    country: Joi.string().required(),
-    director: Joi.string().required(),
-    duration: Joi.number().required(),
-    year: Joi.string().required(),
-    description: Joi.string().required(),
-    image: Joi.string().required().regex(urlRegex),
-    trailerLink: Joi.string().required().regex(urlRegex),
-    thumbnail: Joi.string().required().regex(urlRegex),
-    movieId: Joi.number().required(),
-    nameRU: Joi.string().required(),
-    nameEN: Joi.string().required(),
+  params: Joi.object().keys({
+    movieId: Joi.string().length(24).hex().required(),
   }),
 });
 
